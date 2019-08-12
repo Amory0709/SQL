16fbddefb8e44a87c8f778036606e9bd27780f97
Amory0709 <37529329+Amory0709@users.noreply.github.com>
2019-08-12 19:18:13 +0800

    Update subquery.sql

--- subquery.sql
+++ subquery.sql
@@ -1,3 +1,4 @@
+-- Subquery
 
 SELECT DATE_TRUNC('day', occurred_at) AS day,
 	   channel,
@@ -42,3 +43,100 @@ FROM orders
 WHERE DATE_TRUNC('month',occurred_at) = (SELECT DATE_TRUNC('month',MIN(occurred_at)) AS first_order_month
 	    FROM orders)
 GROUP BY 1;
+
+--Provide the name of the sales_rep in each region with the largest amount of total_amt_usd sales.
+SELECT name, t1.region_id, sale
+FROM (SELECT s.name, 
+             s.region_id, 
+             SUM(o.total_amt_usd) as sale
+      FROM sales_reps s
+      JOIN accounts a
+      ON s.id = a.sales_rep_id
+      JOIN orders o
+      ON a.id = o.account_id
+      GROUP BY 1,2
+      ORDER BY 2) t1 
+JOIN (SELECT region_id, 
+             MAX(sale) as max_sale
+      FROM (SELECT s.name, 
+                   s.region_id, 
+                   SUM(o.total_amt_usd) as sale
+            FROM sales_reps s
+            JOIN accounts a
+            ON s.id = a.sales_rep_id
+            JOIN orders o
+            ON a.id = o.account_id
+            GROUP BY 1,2
+            ORDER BY 2) t3
+      GROUP BY 1) t2
+ON t1.region_id = t2.region_id AND t1.sale = t2.max_sale;
+
+--For the region with the largest (sum) of sales total_amt_usd, how many total (count) orders were placed? 
+SELECT r.name,
+	   SUM(o.total_amt_usd) AS total_amt_usd_sum,
+	   COUNT(o.*) AS order_count
+FROM orders o
+JOIN accounts a
+ON o.account_id = a.id
+JOIN sales_reps s
+ON s.id = a.sales_rep_id
+JOIN region r
+ON r.id = s.region_id
+GROUP BY 1
+HAVING SUM(o.total_amt_usd) = (SELECT MAX(total_amt_usd_sum) FROM (SELECT s.region_id,
+	   SUM(o.total_amt_usd) AS total_amt_usd_sum,
+	   COUNT(o.*) AS order_count
+FROM orders o
+JOIN accounts a
+ON o.account_id = a.id
+JOIN sales_reps s
+ON s.id = a.sales_rep_id
+GROUP BY 1) t1);
+
+--WITH  (CTE)
+
+--Provide the name of the sales_rep in each region with the largest amount of total_amt_usd sales.
+WITH t1 AS
+(SELECT s.name, 
+             s.region_id, 
+             SUM(o.total_amt_usd) as sale
+      FROM sales_reps s
+      JOIN accounts a
+      ON s.id = a.sales_rep_id
+      JOIN orders o
+      ON a.id = o.account_id
+      GROUP BY 1,2
+      ORDER BY 2),
+   t2 AS (SELECT region_id, 
+             MAX(sale) as max_sale
+           FROM t1
+           GROUP BY 1)
+SELECT *
+FROM t1
+JOIN t2
+ON t1.region_id = t2.region_id AND t1.sale = t2.max_sale; 
+
+--For the region with the largest (sum) of sales total_amt_usd, how many total (count) orders were placed? 
+WITH t1 AS (
+   SELECT r.name region_name, SUM(o.total_amt_usd) total_amt
+   FROM sales_reps s
+   JOIN accounts a
+   ON a.sales_rep_id = s.id
+   JOIN orders o
+   ON o.account_id = a.id
+   JOIN region r
+   ON r.id = s.region_id
+   GROUP BY r.name), 
+t2 AS (
+   SELECT MAX(total_amt)
+   FROM t1)
+SELECT r.name, COUNT(o.total) total_orders
+FROM sales_reps s
+JOIN accounts a
+ON a.sales_rep_id = s.id
+JOIN orders o
+ON o.account_id = a.id
+JOIN region r
+ON r.id = s.region_id
+GROUP BY r.name
+HAVING SUM(o.total_amt_usd) = (SELECT * FROM t2);
